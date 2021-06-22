a04f9c664b0ef1be19d1fcea1a7d14d2775db54b
Lavika <lavika.makwana03@gmail.com>
2021-06-22 16:06:05 +0530

    updated title and removed the comments

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,46 +5,7 @@ require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
 
-// import '../stylesheets/application';
-
 window.jQuery = $;
 window.$ = $;
 
 require('packs/main.js');
-
-
-// require('select2/select2.min.js');
-
-// Libraries/Vendor-plugins
-// require('../plugins/jquery/jquery-3.2.1.min.js');
-// require('../plugins/bootstrap/js/popper.js');
-// require('../plugins/bootstrap/js/bootstrap.min.js');
-// require('../plugins/select2/select2.min.js');
-
-// require('../plugins/daterangepicker/moment.min.js');
-// require('../plugins/masonry.min.js');
-// require('../plugins/mmenu.min.js');
-// require('../plugins/tooltips.min.js');
-// require('../plugins/daterangepicker/daterangepicker.js');
-// require('../plugins/js/main.js');
-// require("stylesheets/application.scss")
-// require("@rails/ujs").start()
-// require("turbolinks").start()
-// require("@rails/activestorage").start()
-// require("channels")
-// // import the bootstrap javascript module
-// import 'bootstrap/dist/js/bootstrap'
-// import "bootstrap/dist/css/bootstrap";
-
-// // import the application.scss we created for the bootstrap CSS (if you are not using assets stylesheet)
-// import "../stylesheets/application"
-// var jQuery = require('jquery')
-
-// // include jQuery in global and window scope (so you can access it globally)
-// // in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
-// global.$ = global.jQuery = jQuery;
-// window.$ = window.jQuery = jQuery;
-
-// // const images = require.context('../images', true)
-// // const imagePath = (name) => images(name, true)
-
